274142a5f306655b42d1885645065b2aaf69d4d4
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-30 20:43:21 -0400

    Fix config

--- api/helpers.js
+++ api/helpers.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { UNSPLASH_TOKEN, EDAMAM_APP_ID, EDAMAM_APP_KEY } = require("./config");
+const { UNSPLASH_TOKEN, EDAMAM_APP_ID, EDAMAM_APP_KEY } = require("../config");
 
 const unsplashUrl = `https://api.unsplash.com/developers/?&food`;
 module.exports = {
