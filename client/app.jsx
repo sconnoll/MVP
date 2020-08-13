adc0cffc8591913b83a0e880ee7eb53db8b8967a
Shannon Connolly <shannonconnolly92@gmail.com>
2020-08-13 17:38:23 -0400

    Add react-router to single page app

--- client/app.jsx
+++ client/app.jsx
@@ -3,58 +3,60 @@ import ReactDOM from 'react-dom';
 import SearchRecipes from './components/SearchRecipes';
 import Favorites from './components/Favorites';
 import Future from './components/Future';
-import Form from 'react-bootstrap/Form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: "sign-in",
-      username: "",
-      password: ""
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleClick(e, screen) {
-    this.setState ({
-      view: screen
-    })
-  }
-
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  render() {
-    return (
+const App = (props) => {
+  return (
+    <Router>
       <div className='app-container'>
-        
-        {this.state.view === 'sign-in' ? <>
-          <div className='nav'><span className='logo'><text>My Cookbook</text></span></div>
-          <div className='sign-in'>
-            <label>Username</label>
-            <input type='text' name='username' value={this.state.username} onChange={e => {this.handleChange(e)}}/>
-            <label>Password</label>
-            <input type='password' name='password' value={this.state.password} onChange={e => {this.handleChange(e)}}/>
-            <button id='sign-in-button' onClick={e => this.handleClick(e, 'home')}>Sign In</button>
-          </div></>
-        : <div className='nav'>
-          <span className='logo'><text>{this.state.username}'s Cookbook</text></span>
-          <span className='nav-bar'>
-            <text onClick={e=> this.handleClick(e, 'home')}>Home</text>
-            <text onClick={e=> this.handleClick(e, 'favorites')}>Tried and True</text>
-            <text onClick={e=> this.handleClick(e, 'future')}>Wishlist</text>
-            <text onClick={e=> this.handleClick(e, 'search')}>Explore</text>
-            <text onClick={e=> this.handleClick(e, 'groceries')}>Grocery List</text>
-          </span></div>}
-        {this.state.view === 'home' ? <div><SearchRecipes /><br></br><Favorites /></div> : (this.state.view === 'search' ? <SearchRecipes /> : (this.state.view === 'favorites' ? <Favorites /> : (this.state.view === 'future' ? <Future /> : null)))}
-      
+        <nav>
+          <span className='logo'><text>My Cookbook</text></span>
+          <ul className='nav-bar'>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tried_and_true">Tried and True</Link>
+            </li>
+            <li>
+              <Link to="/wishlist">Wishlist</Link>
+            </li>
+            <li>
+              <Link to="/explore">Explore</Link>
+            </li>
+            <li>
+              <Link to="/groceries">Grocery List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tried_and_true">
+            <Favorites />
+          </Route>
+          <Route path="/wishlist">
+            <Future />
+          </Route>
+          <Route path="/explore">
+            <SearchRecipes />
+          </Route>
+          <Route path="/groceries">
+            {null}
+          </Route>
+          <Route path="/">
+            <div>
+              <SearchRecipes /><br/>
+              <Favorites />
+            </div>
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
+
 ReactDOM.render(<App/>, document.getElementById('app'));
